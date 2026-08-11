42f552cbb1738702a54fa9628c199ac3542031af
Arasu Ramanan <arasuaudi@gmail.com>
2026-08-11 21:34:36 +0530

    Protect UPSI routes with authentication

--- routes/details.routes.js
+++ routes/details.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const Details = require('../models/details.models');
+const { protect } = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
-router.get('/details', async(req, res) => {
+router.get('/details', protect, async (req, res) => {
     try {
         const detail = await Details.find();
         res.send(detail);
@@ -12,11 +13,11 @@ router.get('/details', async(req, res) => {
     }
 });
 
-router.post('/detailsnewform', async(req, res) => {
+router.post('/detailsnewform', protect, async (req, res) => {
     try { 
     const newdetail = new Details(req.body);
     await newdetail.save();
-    res.send("detail Created Successfully");
+    res.send("Detail Created Successfully");
     } catch (error) {
         return res.status(400).json(error);
     }
